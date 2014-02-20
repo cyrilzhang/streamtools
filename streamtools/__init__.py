87d02e323d0fe9ea0015db4312a98e1bfe64b765
Cyril Zhang <cyril.zhang@yale.edu>
2014-02-20 17:57:21 -0500

    more reasonable module structure

---
+++ streamtools/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py of streamtools
+
+from module import *
--- streamtools/module.py
+++ streamtools/module.py
@@ -9,28 +9,35 @@ import scipy.fftpack
 ##### STREAM CLASS #####
 
 class Stream:
+	# constructor
 	def __init__(self, sample_rate_=44100, block_size_=4096):
 		self.sample_rate = sample_rate_
 		self.block_size = block_size_
 		self.blocks_per_second = float(sample_rate_)/block_size_
 
-	def read_block(self): # read mono channel
+	# read mono channel
+	def read_block(self):
 		block = np.fromfile(sys.stdin, dtype=np.int16,count=2*self.block_size)\
 			.reshape(2,self.block_size)
 		return block[0] + block[1]
 
-	def read_spec(self): # magnitude of FFT
+	# magnitude of FFT
+	def read_spec(self):
 		return np.absolute( scipy.fftpack.fft( self.read_block() ) )
 
-	def seconds(self, secs): # seconds to blocks
+	# seconds to blocks
+	def seconds(self, secs):
 		return int(math.ceil( secs * self.blocks_per_second ))
 
-	def hz_to_fft(self, hz): # hertz to index in fft
+	# hertz to index in fft
+	def hz_to_fft(self, hz):
 		return np.clip( int(hz / self.blocks_per_second), 0, self.block_size )
 
-	def fft_to_hz(self, ind): # index in fft to hertz
+	# index in fft to hertz
+	def fft_to_hz(self, ind):
 		return float(ind) * self.blocks_per_second
 
+	# sketchy ASCII art depiction of fft
 	def sketch(self, spec, scale=1.0/8000, minfreq=200, maxfreq=2000, width=80):
 		ret = ''
 		asc = " `.~:<=tIYAXWH#M"
