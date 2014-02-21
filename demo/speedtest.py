5b23856c9382c93bcd92c3c579abfd95853c94a5
Cyril Zhang <cyril.zhang@yale.edu>
2014-02-20 23:02:53 -0500

    finish speed benchmark

--- demo/speedtest.py
+++ demo/speedtest.py
@@ -4,15 +4,35 @@ import sys
 import numpy as np
 import time
 import math
-from subprocess import Popen
 from scipy.fftpack import fft
 
 sys.path = ['..'] + sys.path
 from streamtools import *
 
-stream = Stream()
+def time_fft(secs=10):
+	print "Capturing for", secs, "seconds"
+	stream = Stream()
+	nblocks = stream.seconds(secs)
 
-while True:
-	spec = stream.read_spec()
-	print stream.sketch( spec )
-	# classify(spec)
+	avg_read = 0 # should be 44100 Hz
+	avg_fft = 0 # should be faster than 44100 Hz
+
+	for i in range(nblocks):
+		read_begin = time.time()
+		block = stream.read_block()
+		fft_begin = time.time()
+		spec = np.absolute( fft(block) )
+		end = time.time()
+		avg_read += end - read_begin
+		avg_fft += end - fft_begin
+
+	avg_read /= nblocks
+	avg_fft /= nblocks
+
+	avg_read = stream.block_size / avg_read
+	avg_fft = stream.block_size / avg_fft
+
+	print "Read: %.1lf Hz" % avg_read
+	print "FFT: %.1lf Hz" % avg_fft
+
+time_fft(30)
